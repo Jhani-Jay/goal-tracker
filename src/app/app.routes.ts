83cf4e5847104dfa23cb5e9f5d1f3a81e6b782b4
Jhani-Jay <jaygreat57@gmail.com>
2025-01-01 17:38:33 +0000

    feat: add initial page markup, styles, routes declarations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: '',
+    //     children: [
+    //         {
+    //             path: '',
+    //             loadComponent: () => import('./feature/empty-dashboard/empty-dashboard.component')
+    //                 .then(e => e.EmptyDashboardComponent),
+    //             title: 'Select a goal',
+    //         }
+    //     ]
+    // },
+    {
+        // path: 'goal/:id',
+        path: '',
+        loadComponent: () => import('./feature/goal/goal.component')
+            .then(g => g.GoalComponent),
+        title: 'Goal',
+    }
+];
