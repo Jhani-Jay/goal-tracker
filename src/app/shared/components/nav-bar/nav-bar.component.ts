a504dc2bb9b64ccfb035f9558790e3728b5f65c6
Jhani-Jay <jaygreat57@gmail.com>
2025-01-01 19:25:17 +0000

    feat: add popup menu

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -2,14 +2,45 @@ import { Component } from '@angular/core';
 
 // third party libraries
 import { ButtonModule } from 'primeng/button';
+import { MenuItem, MessageService } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [ButtonModule, ],
+  imports: [ButtonModule, MenuModule],
+  providers: [MessageService],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  items: MenuItem[] | undefined;
 
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+      this.items = [
+          {
+            label: 'Goal options',
+              items: [
+                  {
+                      label: 'Edit goal',
+                      icon: 'pi pi-file-edit'
+                  },
+                  {
+                      label: 'Delete goal',
+                      icon: 'pi pi-trash'
+                  }
+              ]
+          }
+      ];
+  }
+
+  update() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'File created', life: 3000 });
+}
+
+delete() {
+    this.messageService.add({ severity: 'warn', summary: 'Search Completed', detail: 'No results found', life: 3000 });
+}
 }
