72fcb85655b02b4fcbaf7eafb74b4a58dfe6c48a
Jhani-Jay <jaygreat57@gmail.com>
2025-01-02 12:35:05 +0000

    feat: add implementation for dynamically rendering goal and milestone component

---
+++ src/app/core/services/app-service/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  private visible:boolean = false;
+  private isVisibleSubject = new BehaviorSubject<boolean>(this.visible);
+  private isVisible$ = this.isVisibleSubject.asObservable();
+  selectedFormType = signal<string | null>(null);
+
+  constructor() { }
+  
+  showDialog():void {
+    this.visible = true;
+    this.isVisibleSubject.next(this.visible);
+  }
+
+  hideDialog():void {
+    this.visible = false;
+    this.isVisibleSubject.next(this.visible);
+  }
+
+  getVisibility():Observable<boolean> {
+    return this.isVisible$;
+  }
+}
