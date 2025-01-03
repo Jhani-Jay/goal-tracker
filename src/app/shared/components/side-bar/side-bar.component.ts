34fd3292a3d051ccf00da792d5e3958926b60b8e
Jhani-Jay <jaygreat57@gmail.com>
2025-01-03 14:51:35 +0000

    render goals dynamically on the sidebar

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,19 +1,29 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppState } from '@app/core/model/AppState.model';
+import { Goal } from '@app/core/model/goal.model';
 import { AppService } from '@app/core/services/app-service/app.service';
+import { goals } from '@app/core/state/goal.selector';
+import { Store } from '@ngrx/store';
 
 // third party libraries
 import { ButtonModule } from 'primeng/button';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [ButtonModule,],
+  imports: [ButtonModule, AsyncPipe,],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+  goals$:Observable<Goal[]> = this.store.select(goals);
 
   constructor(
+    private router: Router,
+    private store: Store<AppState>,
     private appService: AppService,
   ) {};
 
