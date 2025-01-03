9b573a4a1d709064eb4fdf8caad56e820dfac6bf
Jhani-Jay <jaygreat57@gmail.com>
2025-01-03 10:54:48 +0000

    feat: add ngrx library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 //third party libraries
-import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmationService, } from 'primeng/api';
 
 //local imports
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideAnimations(), 
+    provideRouter(routes),
+    provideAnimations(),
     provideAnimationsAsync(),
     ConfirmationService,
-  ]
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
