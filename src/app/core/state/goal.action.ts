6903ddbb93db1259726de58bce53f4402842690e
Jhani-Jay <jaygreat57@gmail.com>
2025-01-03 14:27:53 +0000

    add CRUD actions and reducers for goal

--- src/app/core/state/goal.action.ts
+++ src/app/core/state/goal.action.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { Goal } from '../model/goal.model';
 
+// GET actions
 export const onLoadGoals = createAction('[Goal] load goals');
 export const onLoadGoalSuccess = createAction(
     '[Goal] load goals successfully',
@@ -10,3 +11,21 @@ export const onLoadGoalFailure = createAction(
     '[Goal] load goals failed',
     props<{error: string}>()
 );
+
+// POST action
+export const onPostGoal = createAction(
+    '[Goal] post goal',
+    props<{goal: Goal}>(),
+);
+
+// EDIT action
+export const onUpdateGoal = createAction(
+    '[Goal] update goal',
+    props<{goal: Partial<Goal>}>(),
+);
+
+// DELETE action
+export const onDeleteGoal = createAction(
+    '[Goal] delete goal',
+    props<{id: string}>(),
+)
--- src/app/core/state/goal.reducer.ts
+++ src/app/core/state/goal.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
-import { onLoadGoals, onLoadGoalSuccess, onLoadGoalFailure } from './goal.action';
+import { onLoadGoals, onLoadGoalSuccess, onLoadGoalFailure, onPostGoal, onUpdateGoal, onDeleteGoal } from './goal.action';
 import { Goal } from '../model/goal.model';
 
 // EntityState is a generic interface that takes the type of the entity as a type parameter
@@ -18,5 +18,14 @@ export const goalReducer = createReducer(
     initialState,
     on(onLoadGoals, state => state),
     on(onLoadGoalSuccess, (state, {goals}) => goalAdapter.setAll(goals, state)),
+    on(onPostGoal, (state, {goal}) => goalAdapter.addOne(goal, state)),
+    on(onUpdateGoal, (state, {goal}) => {
+        const { id } = goal;
+        if (id) {
+            return goalAdapter.updateOne({id: id, changes: goal}, state)
+        };
+        return state;
+    }),
+    on(onDeleteGoal, (state, {id}) => goalAdapter.removeOne(id, state)),
     on(onLoadGoalFailure, (state, {error}) => ({...state, error})),
 )
